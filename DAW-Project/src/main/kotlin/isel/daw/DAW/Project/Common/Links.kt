10e3d9312c7e5083db350bcbeeba91734da07778
Ari <arivalente99@gmail.com>
2020-03-16 12:27:38 +0000

    Diretoria Common-> Links e MediaType

---
+++ DAW-Project/src/main/kotlin/isel/daw/DAW/Project/Common/Links.kt
@@ -0,0 +1,30 @@
+package isel.daw.DAW.Project.Common
+
+/**
+ * All paths on the API
+ * The ones with a token that can have multiple values (starting with : ) are commented
+ * because we are not sure if this is the right sintax
+ */
+
+/*PROJECT PATHS*/
+const val PROJECTS_PATH = "/projects"
+const val GET_PROJECTS_PATH = "${PROJECTS_PATH}/getprojects"
+const val GET_SINGLE_PROJECT_PATH = "${PROJECTS_PATH}/getproject"
+const val CREATE_PROJECT_PATH = "${PROJECTS_PATH}/createproject"
+const val UPDATE_PROJECT_PATH = "${PROJECTS_PATH}/updateproject"
+const val DELETE_PROJECT_PATH = "${PROJECTS_PATH}/deleteproject"
+
+/*ISSUE PATHS*/
+const val ISSUES_PATH = "/issues"
+const val GET_ISSUES_PATH = "${ISSUES_PATH}/getissues"
+const val GET_SINGLE_ISSUE_PATH = "${ISSUES_PATH}/getissue"
+const val CREATE_ISSUE_PATH = "${ISSUES_PATH}/createissue"
+//const val UPDATE_ISSUE_PATH = "${ISSUES_PATH}/updateissue/:tid"
+//const val UPDATE_ISSUE_STATE_PATH = "${UPDATE_ISSUE_PATH}/updatestate"
+//const val DELETE_ISSUE_PATH = "${ISSUES_PATH}/deleteissue/:tid"
+
+/*COMMENT PATHS*/
+const val COMMENTS_PATH = "/comments"
+//const val GET_COMMENTS_PATH = "${COMMENTS_PATH}/getcomments/:tid"
+//const val CREATE_COMMENT_PATH = "${COMMENTS_PATH}/createcomment/:tid"
+//const val DELETE_COMMENT_PATH = "${COMMENTS_PATH}/deletecomment/:cid"
---
+++ DAW-Project/src/main/kotlin/isel/daw/DAW/Project/Common/MediaTypes.kt
@@ -0,0 +1,3 @@
+package isel.daw.DAW.Project.Common
+
+const val APPLICATION_JSON = "application/json"
