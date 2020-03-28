7efc1ec5ba447a52e7a56cfb88def2f0b6fba2e0
AriValente <arivalente99@gmail.com>
2020-03-28 20:51:44 +0000

    Um algoritmo meio puxado

--- DAW-Project/src/main/kotlin/isel/daw/DAW/Project/Projects/ProjectsRepository.kt
+++ DAW-Project/src/main/kotlin/isel/daw/DAW/Project/Projects/ProjectsRepository.kt
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
+import java.lang.Error
+import java.util.*
 import javax.sql.DataSource
 
 /**
@@ -30,7 +32,25 @@ class ProjectsRepository(@Autowired val dbs: DataSource) {
     }
 
     fun create( newProject: ProjectsInputModel) {
-        validateTrasitions(newProject)
+        //validateTrasitions(newProject)
+        /**
+         * Testing my, far superior, methods heheheh
+         */
+        val result1 = verifyGraph(
+                graphInit(arrayOf( Pair("A","B") , Pair("B","A") , Pair("A","C") , Pair("C","closed") , Pair("closed","C") , Pair("closed","archived"))),
+                "A"
+        )
+
+        val result2 = verifyGraph(
+                graphInit(arrayOf( Pair("A","B") , Pair("A","C") , Pair("C","closed") , Pair("closed","C") , Pair("closed","archived"))),
+                "A"
+        )
+
+        val result3 = verifyGraph(
+                graphInit(arrayOf( Pair("A","B") , Pair("B","D"), Pair("D", "closed") , Pair("A","C") , Pair("A", "E"), Pair("E", "A"), Pair("C","closed") , Pair("closed","C") , Pair("closed", "D"), Pair("closed","archived"))),
+                "A"
+        )
+
         return CreateProject.execute(newProject, dbs.connection)
     }
 
@@ -89,6 +109,64 @@ class ProjectsRepository(@Autowired val dbs: DataSource) {
                 }
             }
             return false
+        }
+
+
+        /**
+         * Assim é que se faz pah!!! :
+         */
+
+        fun graphInit(transitions: Array<Pair<String, String>>): MutableMap<String , MutableList<String>> {
+            val graph : MutableMap<String, MutableList<String>> = mutableMapOf()
+
+            for(s in transitions){
+                if(s.first == s.second) throw Error("Assim nao dá oh maninho, metes-me uma transiçao que vai para ela propria?!")
+                if(!graph.containsKey(s.first)) {
+                    graph.put(s.first, mutableListOf())
+                }
+                graph.get(s.first)?.add(s.second)
+            }
+
+            return graph
+        }
+
+        fun verifyGraph(graph : MutableMap<String , MutableList<String>>, INITIAL_STATE: String): Boolean{
+            val initialNexts : MutableList<String> = graph.get(INITIAL_STATE)?:return false
+            var PROGRESSO: String = "POR AQUI N SEI BEM FDZ"
+            for( s in initialNexts ) {
+                var aux = verifyAux(INITIAL_STATE, s, graph)
+                if(aux == "TOU PRESO AQUI OH MANO") return false
+                if(PROGRESSO == "POR AQUI N SEI BEM FDZ") {
+                    PROGRESSO = aux
+                }
+            }
+            when(PROGRESSO) {
+                "POR AQUI N SEI BEM FDZ" -> return false
+                "AQUI DA CRLH" -> return true
+                else -> return false
+            }
+        }
+
+        fun verifyAux(called: String, toCheck: String, graph: MutableMap<String, MutableList<String>>) : String{
+            val nexts: List<String> = graph.get(toCheck)?: return "TOU PRESO AQUI OH MANO"
+            var PROGRESSO = "POR AQUI N SEI BEM FDZ"
+            if(nexts.isEmpty()) return "TOU PRESO AQUI OH MANO"
+            for( s in nexts ) {
+                when(s) {
+                    called -> PROGRESSO = "POR AQUI N SEI BEM FDZ"
+                    "archived" -> PROGRESSO = "AQUI DA CRLH"
+                    else -> {
+                        val aux = verifyAux(toCheck, s, graph)
+                        if(aux == "TOU PRESO AQUI OH MANO") {
+                            return "TOU PRESO AQUI OH MANO"
+                        }
+                        if(PROGRESSO == "POR AQUI N SEI BEM FDZ") {
+                            PROGRESSO = aux
+                        }
+                    }
+                }
+            }
+            return PROGRESSO
         }
     }
 }
